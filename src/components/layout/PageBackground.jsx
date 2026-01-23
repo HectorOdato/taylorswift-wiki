415a5cbddf10c4e373dc0c2b760188d6d883bf3e
HectorOdato <leoodato@gmail.com>
2026-01-22 23:05:35 -0300

    modificacion del background de cada pagina segun los colores de cada era, se impleneta para esto un nuevo componente PageBackground.jsx

---
+++ src/components/layout/PageBackground.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const gradientClasses = {
+  debut: "bg-gradient-to-b from-black via-green-900/70 to-black",
+  fearless: "bg-gradient-to-b from-black via-yellow-900/70 to-black",
+  speakNow: "bg-gradient-to-b from-black via-purple-900/70 to-black",
+  red: "bg-gradient-to-b from-black via-red-900/70 to-black",
+  nineteenEightyNine: "bg-gradient-to-b from-black via-blue-900/70 to-black",
+  reputation: "bg-gradient-to-b from-black via-green-900/70 to-black",
+  lover: "bg-gradient-to-b from-black via-pink-900/70 to-black",
+  folklore: "bg-gradient-to-b from-black via-gray-900/70 to-black",
+  evermore: "bg-gradient-to-b from-black via-indigo-900/70 to-black",
+  midnights: "bg-gradient-to-b from-black via-violet-900/70 to-black",
+  torturedPoetsDepartment: "bg-gradient-to-b from-black via-teal-900/70 to-black",
+  lifeOfAShowgirl: "bg-gradient-to-b from-black via-cyan-900/70 to-black"
+};
+
--- src/pages/AlbumPage.jsx
+++ src/pages/AlbumPage.jsx
@@ -16,11 +16,11 @@ function AlbumPage() {
 
   return (
     <div className="min-h-screen relative bg-black">
-      <Header className="z-15 relative" />
-      <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-black via-orange-900/70 to-black z-0"></div>
+      <Header className="relative z-10" />
+      <div className={`absolute inset-0 ${album.background} z-0`} ></div>
       <div className="max-w-4xl mx-auto p-4 z-10 relative">
         <h1 className="text-3xl font-bold mb-4">{album.title} ({album.año})</h1>
-        <img src={album.img} alt={album.title} className="w-full h-auto mb-4 rounded-lg" />
+        <img src={album.banner} alt={album.title} className="w-full h-auto mb-4 rounded-lg" />
         <p className="mb-4">{album.descripcion}</p>
         <h2 className="text-2xl font-semibold mb-4">Canciones:</h2>
         <div className="space-y-2">
